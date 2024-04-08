494cbd689a9c5e6d13c1b45e8428d3f358f34413
jels7 <cucchiaa@oregonstate.edu>
2024-04-08 11:39:02 -0700

    Bag of marbles

--- Assignment_1.cpp
+++ Assignment_1.cpp
@@ -3,17 +3,34 @@ using namespace std;
 
 int main() {
     std::cout << "We have an empty bag of marbles."<<std::endl;
+    //This program will keep track of the number of marbles in a bag.
+
     int bagofmarbles = 0;
+    //This sets the number of marbles in the bag to 0.
+    
     cout << bagofmarbles << " marbles" << endl;
     std::cout << "We add 2 yellow marbles to the bag."<<std::endl;
+    //This displays the number of marbles in the bag.
+
     bagofmarbles = bagofmarbles + 2;
+    //This adds 2 marbles to the bag.
+
     cout << bagofmarbles << " marbles" << endl;
+    //This displays the number of marbles in the bag.
+
     std::cout << "We add 3 blue marbles to the bag."<<std::endl;
     bagofmarbles = bagofmarbles + 3;
+    //This adds 3 marbles to the bag.
+
     cout << bagofmarbles << " marbles" << endl;
     std::cout << "We remove 1 yellow marble from the bag."<<std::endl;
+
     bagofmarbles = bagofmarbles - 1;
+    //This removes 1 marble from the bag.
+
     cout << bagofmarbles << " marbles" << endl;
     cout << "We started with 0 marbles and now we have " << bagofmarbles << " marbles." << endl;
+    //This displays the number of marbles in the bag.
+    
     return 0;
 }
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-This is a readme file.....
+This is a simple program with a bag of marbles that shows what happens when adding and taking away marbles from the bag.
+
+Variables: bagofmarbles
+This variable represents the amount of marbles in the bag and is updated when marbles are added or taken away.
+
+Marbles are added and taken away by using operators + -
+Using a print statment shows us that the bag of marbles is actually growing or decreasing, which means that the code is functioning as intended.
